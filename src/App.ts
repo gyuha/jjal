84b2140a3f9941400d74523522e47cf07f008936
gyuha <nicegyuha@gmail.com>
2019-12-07 23:21:12 +0900

    검색시 로딩 추가

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,6 @@
 import Card from '@/components/Card';
-import { image_search_generator } from 'duckduckgo-images-api';
+import JJAL from '@/graphql/query/jjal.gql';
+import { SmartQuery } from 'vue-apollo-decorator';
 import { Component, Vue } from 'vue-property-decorator';
 import WithRender from './App.html';
 import getConfig from './_config';
@@ -21,43 +22,34 @@ interface IImages {
   },
 })
 export default class App extends Vue {
-  private images: IImages[] = [];
+    @SmartQuery({
+        query: JJAL,
+        variables() {
+          return {
+            q: this.query,
+          };
+        },
+      })
+    private jjal!: IImages[];
+    private query: string = '';
 
-  private searchText: string = '';
+    private searchText: string = '';
 
-  private defaultJjal: string[] = [
-    '펭수', '최신', '유행', '개구리',
-  ];
-
-  get title(): string {
-    return getConfig().title;
-  }
-
-  private mounted() {
-    this.searchText = this.defaultJjal[
-      Math.floor(Math.random() * (this.defaultJjal.length - 1))
+    private defaultJjal: string[] = [
+      '펭수', '최신', '유행', '개구리',
     ];
-    this.getImages();
-  }
 
-  private async getLocalImages() {
-     const images = await this.$http.get('//localhost:3000?q=' + this.searchText);
-     this.images = images.data;
-  }
-
-  private async getImages() {
-    if (process.env.NODE_ENV === 'development') {
-      this.getLocalImages();
-      return;
+    get title(): string {
+      return getConfig().title;
     }
 
-    this.images = [];
-    const query = '짤 ' + this.searchText;
-    for await (const resultSet of await image_search_generator({ query, moderate: true , iterations : 1})) {
-      // @ts-ignore
-      resultSet.forEach((element: Images) => {
-        this.images.push(element);
-      });
+    private mounted() {
+      this.query = this.searchText = this.defaultJjal[
+        Math.floor(Math.random() * (this.defaultJjal.length - 1))
+      ];
     }
-  }
+
+    private getImages() {
+      this.query = this.searchText;
+    }
 }
